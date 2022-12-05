5258b3035085bac05bf1ce54c8274c0c33848647
ArtemTuba <tuba9521@gmail.com>
2022-12-05 18:30:54 +0200

    deleting the console.log at work

--- main.js
+++ main.js
@@ -3,22 +3,16 @@ operation = operation.toLowerCase();
 console.log(operation);
 
 const firstNum = parseInt(prompt('Введіть перше число!'));
-console.log(firstNum);
 const secondNum = parseInt(prompt('Введіть друге число!'));
-console.log(secondNum);
 
 if(operation === 'add' && firstNum >= 0 && secondNum >= 0) {
     alert(`"${firstNum} + ${secondNum} = ${firstNum + secondNum}"`);
-    console.log(firstNum + secondNum);
 } else if(operation === 'sub' && firstNum >= 0 && secondNum >= 0){
     alert(`"${firstNum} - ${secondNum} = ${firstNum - secondNum}"`);
-    console.log(firstNum - secondNum);
 } else if(operation === 'mult' && firstNum >= 0 && secondNum >= 0){
     alert(`"${firstNum} * ${secondNum} = ${firstNum * secondNum}"`);
-    console.log(firstNum * secondNum);
 } else if(operation === 'div' && firstNum >= 0 && secondNum >= 0){
     alert(`"${firstNum} / ${secondNum} = ${firstNum / secondNum}"`);
-    console.log(firstNum / secondNum);
 } else {
     alert('Спробуйте ще раз вірно ввести операцію калькуляції!');
 }
